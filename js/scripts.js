73d173719bf356a367c1096fbe6ffb1ccda1052f
Jen Bruce and Karla Carballo <jen@>
2019-02-18 20:46:21 -0800

    Add initial html, js css, for superhero dating site.

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+  $("form#datingParameters").submit(function(event) {
+    var age = $("select#age").val();
+    var gender = $("select#gender").val();
+    var color = $("select#color").val();
+    var animal = $("select#animal").val();
+
+    if (age === 'first' && gender === 'female') {
+      //var quote = (100 - age) * 3;
+
+      // if (gender === 'male' && age < 26) {
+      //   quote += 50;
+      // }
+      var 
+      $('#superheroName').
+      $("#superheroDate").show();
+    }
+
+      //$("#rate").empty().append(quote);
+
+    // } else {
+    //   alert('Please enter your age.');
+    // }
+
+
+
+    event.preventDefault();
+  });
+});
